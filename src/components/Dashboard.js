b1dae3a25907505ad8bfb7d4c8d18387549deb70
xidan-developer <mezencev.daniil@icloud.com>
2022-05-30 17:56:07 +0700

    MVP2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,18 +2,23 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "./Auth";
 import firebaseConfig from "../config.js";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import Container from "@mui/material/Container";
 
 const Dashboard = () => {
     const { currentUser } = useContext(AuthContext);
     if (!currentUser) {
         return <Navigate to="/login" />;
     }
+    const theme = createTheme();
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p>This is the dashboard, if you can see this you're logged in.</p>
-            <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xxl">
+                <h1>Главная страница</h1>
+                <p>Вы успевно авторизировались и пошли на страницу</p>
+                <button onClick={() => firebaseConfig.auth().signOut()}>Выйти</button>
+            </Container>
+        </ThemeProvider>
     );
 };
 
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,6 +3,20 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "./Auth";
 import firebaseConfig from "../config.js";
 
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 const LogIn = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,20 +28,78 @@ const LogIn = () => {
         }
     };
     const { currentUser } = useContext(AuthContext);
+    const theme = createTheme();
     if (currentUser) {
         return <Navigate to="/dashboard" />;
     }
     return (
-        <>
-            <h1>Авторизация</h1>
-            <form onSubmit={handleSubmit}>
-                <label for="email">Email</label>
-                <input type="email" name="email" placeholder="Email" />
-                <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Submit</button>
-            </form>
-        </>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Авторизация
+                    </Typography>
+
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Запомнить пароль"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Войти
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Восстановить пароль
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"У вас нет аккаунта? Регистрация"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 };
 
